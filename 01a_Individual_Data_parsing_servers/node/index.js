73efa9155244b32c8d1efae710517044deb45cd5
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-02-05 16:06:16 +0100

    playing with parsers, but package type have problems. Bug: require or import don't work

---
+++ 01a_Individual_Data_parsing_servers/node/index.js
@@ -0,0 +1,27 @@
+const fs = require('node:fs');
+
+// XML - parsering here 
+
+import { XMLValidator } from 'fast-xml-parser';
+
+// read the content of the file
+const xmlFileRawData = "";
+
+fs.readFile('./data/me.xml', 'utf8', (err, rawData) => {
+    if (err) throw err;
+    console.log(err);
+    
+    console.log(rawData);
+    // xmlFileRawData = rawData;
+})
+
+const dataXmlCheck = XMLValidator.validate(xmlFileRawData);
+
+if (dataXmlCheck === true) {
+    console.log('xml file is valid',  dataXmlCheck);
+}
+if (dataXmlCheck.err) {
+    console.log('xml is invalid because of - ' + dataXmlCheck.err.msg);
+}
+
+// end of - XML - parsering here 

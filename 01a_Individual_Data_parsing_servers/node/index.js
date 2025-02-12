8d53866fad5110a6d87af5d0e38ee077037ef6af
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-02-12 22:52:35 +0100

    node pasering of xml, json, csv.

--- 01a_Individual_Data_parsing_servers/node/index.js
+++ 01a_Individual_Data_parsing_servers/node/index.js
@@ -5,69 +5,138 @@ import fs from "fs";
 // XML - parsering here
 
 import { XMLValidator , XMLParser, XMLBuilder } from 'fast-xml-parser';
+import Papa from 'papaparse';
 
 async function readXmlFile(fullFilePath) {
-    try{
-        fs.readFile(fullFilePath, 'utf8', (err, data) => {
-            if (err) {
-              console.error(err);
-              return;
-            }
-            // console.log("data", data);
 
-            // fix this shit later, this is just so fucking bad code
-
-            parsingXml(data);
-
-            return data.toString();
-          });
-    } catch(error) {
-        console.error("error", error);
-    }
+    return new Promise((resolve, reject) => {
+        try{
+            fs.readFile(fullFilePath, 'utf8', (err, data) => {
+                if (err) {
+                  console.error(err);
+                  reject(err);
+                }
+                // console.log("data", data);
+                // TODO
+                resolve(data.toString());
+              });
+        } catch(error) {
+            console.error("error", error);
+        }
+    })
 }
 
-async function parsingXml(input) {
-    console.log("input", input);
-      const parser = new XMLParser();
-
-      let jObj = parser.parse(input);
-
-      const builder = new XMLBuilder();
-      //const xmlContent = builder.build.apply(jObj);
-
-      console.log(jObj);
-
-      return jObj;
-}
-
-async function parsingJson(input) {
-    // test and check if it is a valid json
-    let obj = JSON.parse(input);
-    console.log(obj);
-    return obj;
+async function readCsvFile(fullFilePath) {
+    // promise is awaitable
+    // is only done when "resolve" or "reject" are runned
+    return new Promise((resolve, reject) => {
+        try{
+            fs.readFile(fullFilePath, 'utf8', (err, data) => {
+                if (err) {
+                  console.error(err);
+                  reject(err);
+                }
+                //console.log("data", data);
+                resolve(data.toString());
+              });
+        } catch(error) {
+            console.error("error", error);
+            reject(error);
+        }
+    })
 }
 
 async function readJsonFile(fullFilePath) {
-    try{
-        fs.readFile(fullFilePath, 'utf8', (err, data) => {
-            if (err) {
-              console.error(err);
-              return;
-            }
-            // console.log("data", data);
-
-            // fix this shit (json) later, this is just so fucking bad code
-
-            parsingJson(data);
-
-            return data.toString();
-          });
-    } catch(error) {
-        console.error("error", error);
-    }
+    return new Promise((resolve, reject) => {
+        try{
+            fs.readFile(fullFilePath, 'utf8', (err, data) => {
+                if (err) {
+                  console.error(err);
+                  reject(err);
+                }
+                // console.log("data", data);
+    
+                resolve(data.toString());
+              });
+        } catch(error) {
+            console.error("readJsonFile error", error);
+            reject(error);
+        }
+    })
 }
 
-// console.log(parsingXml(readFile('./data/me.xml')));
+async function parsingCsv(input) {
+    return new Promise((resolve, reject) => {
+        try {
+            let output = Papa.parse(input, (e) => {
+                //console.log("e", e);
+            });
+            console.log("csvParsing", output);
 
-//let xmlOutput = await readXmlFile('./data/me.xml');
-let jsonOutput = await readJsonFile('./data/me.json');
+            // convert it to json data
+            // TODO - make this cleaner
+            let raw = output.data;
+            
+            resolve(raw);
+        } catch (error) {
+            console.log("parsingCsv error",error);
+            reject(error);
+        }
+    })
+}
+
+async function parsingXml(input) {
+    return new Promise((resolve, reject) => {
+        try {
+            console.log("input", input);
+            const parser = new XMLParser();
+      
+            let jObj = parser.parse(input);
+      
+            const builder = new XMLBuilder();
+            //const xmlContent = builder.build.apply(jObj);
+      
+            console.log(jObj);
+      
+            resolve(jObj);
+          }
+          catch (error) {
+              console.error("parsingXml error",error);
+              reject(error);
+          }
+    })
+}
+
+async function parsingJson(input) {
+    return new Promise((resolve, reject) => {
+        try{
+            // test and check if it is a valid json
+            let obj = JSON.parse(input);
+            console.log(obj);
+            resolve(obj);
+        }catch(error) {
+            console.log("parsingJson error",error);
+            reject(error);
+        }
+    })
+}
+
+
+// xml 
+let xmlData = await readXmlFile("./data/me.xml");
+let xmlParsed = await parsingXml(xmlData);
+console.log("xmlParsed",xmlParsed["note"]); // TODO - fix this shit
+console.log("---------------------------------------------------------");
+
+// json
+let jsonData = await readJsonFile("./data/me.json");
+let jsonParsed = await parsingJson(jsonData);
+console.log("jsonParsed", jsonParsed);
+console.log("---------------------------------------------------------");
+
+// csv
+let csvData = await readCsvFile('./data/me.csv');
+let csvParsed = await parsingCsv(csvData);
+console.log("csvParsed", csvParsed); 
+console.log("---------------------------------------------------------");
+

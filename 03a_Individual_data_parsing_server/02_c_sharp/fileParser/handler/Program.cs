b2e4ff77a2e8a1282285e51d00efb47d76439e5b
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-03-26 21:26:02 +0100

    implemented a Person class. Added Parser functions for all 5 types. implemented the ParseYaml function.

--- 03a_Individual_data_parsing_server/02_c_sharp/fileParser/handler/Program.cs
+++ 03a_Individual_data_parsing_server/02_c_sharp/fileParser/handler/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using YamlDotNet.Core;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
 string GetTextFromFIle(string localFolderPath) 
 {
@@ -20,6 +22,46 @@ string GetTextFromFIle(string localFolderPath)
     return text;
 }
 
+Person ParseXml(string input)
+{
+    Console.WriteLine("parseXml");
+    return new Person("2", 2, ["s"]);
+}
+
+Person Parsejson(string input)
+{
+    Console.WriteLine("parseJson");
+    return new Person("2", 2, ["s"]);
+}
+
+
+Person ParseYaml(string input)
+{
+    // I'm using YamlDotNet
+    // https://github.com/aaubry/YamlDotNet
+    
+    // we have the file data as "input", now we will convert it to an obj
+    
+    // the builder class, that handles all the logic
+    var deserializer = new DeserializerBuilder().Build();
+    
+    // the Derserialize will try to typecast our string as a "Person" object.
+    var p = deserializer.Deserialize<Person>(input);
+    
+    return p;
+}
+
+Person ParseTxt(string input)
+{
+    Console.WriteLine("parseTxt");
+    return new Person("2", 2, ["s"]);
+}
+
+Person ParseJson(string input)
+{
+    Console.WriteLine("parseJson");
+    return new Person("2", 2, ["s"]);
+}
 
 string csvText = GetTextFromFIle("/data/me.csv");
 string xmlText = GetTextFromFIle("/data/me.xml");
@@ -29,22 +71,55 @@ string yamlText = GetTextFromFIle("/data/me.yaml");
 
 
 
-Console.WriteLine("-----------------------------------------");
-Console.WriteLine("txtText");
-Console.WriteLine(txtText);
+// Console.WriteLine("-----------------------------------------");
+// Console.WriteLine("txtText");
+// Console.WriteLine(ParseTxt(txtText));
 
-Console.WriteLine("-----------------------------------------");
-Console.WriteLine("xmlText");
-Console.WriteLine(xmlText);
+// Console.WriteLine("-----------------------------------------");
+// Console.WriteLine("xmlText");
+// Console.WriteLine(xmlText);
 
-Console.WriteLine("-----------------------------------------");
-Console.WriteLine("jsonText");
-Console.WriteLine(jsonText);
+// Console.WriteLine("-----------------------------------------");
+// Console.WriteLine("jsonText");
+// Console.WriteLine(jsonText);
 
-Console.WriteLine("-----------------------------------------");
-Console.WriteLine("csvText");
-Console.WriteLine(csvText);
+// Console.WriteLine("-----------------------------------------");
+// Console.WriteLine("csvText");
+// Console.WriteLine(csvText);
 
 Console.WriteLine("-----------------------------------------");
 Console.WriteLine("yamlText");
-Console.WriteLine(yamlText);
+Person tempP = ParseYaml(yamlText);
+Console.WriteLine(tempP.printPerson());
+
+public class Person
+{
+    public string name;
+    public int age;
+    public string[] hobbies;
+
+    public Person(string name, int age, string[] hobbies)
+    {
+        this.name = name;
+        this.age = age;
+        this.hobbies = hobbies;
+    }
+
+    public Person()
+    {
+        
+    }
+
+    public string printPerson()
+    {
+        string allHobbies = "[";
+        foreach (string hobby in hobbies)
+        {
+            allHobbies += "'" + hobby + "'";
+        }
+
+        allHobbies += "]";
+        
+        return name + ", " + age + ", " + allHobbies;
+    }
+}

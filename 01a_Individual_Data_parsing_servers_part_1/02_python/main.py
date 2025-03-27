448bede9718a303386fab0c8dce6dcff230be661
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-03-28 00:55:50 +0100

    01a - python - implemented Person class and parse functions for xml, yaml, csv, json

--- 01a_Individual_Data_parsing_servers_part_1/02_python/main.py
+++ 01a_Individual_Data_parsing_servers_part_1/02_python/main.py
@@ -1,87 +1,108 @@
 
 import csv
 import xml.etree.ElementTree as ET # native libarry,  # https://docs.python.org/3/library/xml.etree.elementtree.html
-# from ruamel.yaml.main import round_trip_load as yaml_load
-import yaml
+from ruamel.yaml import YAML
 import json
 
-print("------------- start of XML ------------------------------")
+
+class Person:
+    name: str
+    age: int
+    hobbies: list[str]
+
+    def __init__(self, name: str, age: int, hobbies: list[str]):
+        self.name = name
+        self.age = age
+        self.hobbies = hobbies
+    
+    def print(self):
+        return "name: " + self.name + " , age: " + str(self.age) + " , hobbies: " + ', '.join(self.hobbies)
+
+
+def readFromFile(path: str):
+    # TODO - try and catch, since path could be wrong or the file is gone
+    output = open(path, "r")
+    return output.read()
+
+
+def ParseJson(input: str):
+    data = json.loads(input)
+
+    return Person(data["name"], data["age"], data["hobbies"])
+
+
+def ParseYaml(input: str):
+    # https://yaml.dev/doc/ruamel.yaml/example/#top 
+
+    yaml = YAML()
+    data = yaml.load(input)
+
+    return Person(data['name'], data['age'], data['hobbies'])
+
+
+def ParseCsv(path: str):
+
+    heads = ""
+    #elements = []
+    element = ""
+    first = False
+
+    with open(path, newline='', encoding='utf-8') as f:
+        reader = csv.reader(f, delimiter=",")
+        for row in reader:
+            if(first == False):
+                heads = row
+                first = True
+            else:
+                element = row
+
+    # TODO - handle more than one obj - this program only handle one obj, but these files will have more than one obj in them    
+    hobbies = element[2].split(";")
+
+    return Person(element[0], element[1], hobbies)
+
+def ParseXml(input: str):
+    root = ET.fromstring(input)
+
+    # print(root.find("name").text, root.find("age").text, len(root.find("hobbies")))
+
+    hobbies: list[str] = []
+
+    for child in root.find("hobbies"):
+        hobbies.append(child.text)
+
+    return Person(root.find("name").text, root.find("age").text, hobbies)
+
+def ParseTxt(input: str):
+    return 0
+
 
 # xml
-file_xml_reading = open("./data/me.xml", "r")
-# print(file_xml_reading)
+print("------------- XML parse ------------------------------")
+xmlText = readFromFile("./data/me.xml")
+xmlPerson: Person = ParseXml(xmlText)
+print(xmlPerson.print())
 
-tree = ET.parse(file_xml_reading)
-root = tree.getroot()
-
-# tree.getroot()
-# https://scrapingant.com/blog/python-parse-xml#using-xmletreeelementtree-for-efficient-xml-parsing
-for child in root:
-    print(child.tag, ":", child.text)
-    # TODO - fix hobbies
-
-# TODO - change this to some kind of usable obj
-
-print("------------- end of XML ------------------------------")
 
 # json
-print("------------- start of Json ------------------------------")
-
-file_json_reading = open("./data/me.json", "r")
-parse_json = json.loads(file_json_reading.read())
-print("json", parse_json)
-
-print("------------- end of Json ------------------------------")
-
-# txt
-print("------------- start of txt ------------------------------")
-
-print("------------- end of txt ------------------------------")
+print("------------- Json parse ------------------------------")
+jsonText = readFromFile("./data/me.json")
+jsonPerson: Person = ParseJson(jsonText)
+print(jsonPerson.print())
 
 # yaml
-print("------------- start of yaml ------------------------------")
-# pyyaml dead ? https://stackoverflow.com/questions/75850232/pyyaml-looks-like-is-outdated
-# only give support up to YAML 1.1, but the newest version is at least 1.2 
-
-# ruamel.yaml is giving some problems
-# ruamel.yaml better?   https://pypi.org/project/ruamel.yaml/
-# guide https://medium.com/towards-data-science/writing-yaml-files-with-python-a6a7fc6ed6c3
-
-# lets make a data class - # https://yaml.dev/doc/ruamel.yaml/dumpcls/#top
-
-# class yaml_me_data:
-#     def __init__(self, name, age, hobbies):
-#         self.name = name
-#         self.age = age
-#         self.hobbies = hobbies
-
-# yaml = YAML()
-# yaml.default_flow_style = False # is this really needed ?
-# print("yaml", yaml)
-# file_yaml_reading = open("./data/me.yaml", "r")
-
-# https://python.land/data-processing/python-yaml#:~:text=The%20most%20used%20python%20YAML,you%20to%20work%20with%20JSON.
-
-file_yaml_reading = open("./data/me.yaml", "r")
-result = yaml.safe_load(file_yaml_reading.read())
-print("yaml", result)
-print("yaml hobbies", result["hobbies"])
-
-
-print("------------- end of yaml ------------------------------")
+print("------------- yaml parse ------------------------------")
+yamlText = readFromFile("./data/me.yaml")
+yamlPerson: Person = ParseYaml(yamlText)
+print(yamlPerson.print())
 
 # csv 
 print("------------- start of csv ------------------------------")
-# https://www.digitalocean.com/community/tutorials/parse-csv-files-in-python
 
-with open('./data/me.csv', 'r') as csv_file: 
-    reader = csv.reader(csv_file)
+# csvText = readFromFile("./data/me.csv")
+# here we send path to parseCsv (because I can't get csv to work with string right now)
+csvPerson: Person = ParseCsv("./data/me.csv")
+print(csvPerson.print())
 
-    for row in reader:
-        print(row)
-    csv_file.close()
-
-    # TODO - handle this a obj
-    # this looks like ass, fix it
-
-print("------------- end of csv ------------------------------")
+# txt
+#print("------------- txt parse ------------------------------")

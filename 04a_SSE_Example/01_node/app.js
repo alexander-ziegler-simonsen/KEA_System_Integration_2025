bb10c8295c46069ed9bde270df6290b1a2808843
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-03-28 09:57:45 +0100

    04a - node - small chnages and a lot of styling

--- 04a_SSE_Example/01_node/app.js
+++ 04a_SSE_Example/01_node/app.js
@@ -1,10 +1,14 @@
 import express from "express";
 
+// app er en instance af express - pt ikke startet op
 const app = express();
 
+// express kan host static filer (html, css, osv)
+// her fortæller vi med : express.static("public") - at vores 'root' path er 'public'
+// alt i denne folder bliver loadet  og hostet med express
 app.use(express.static("public"));
-// console.log(process.env.PORT);
 
+// GET      endpoint       
 app.get("/synchronizetime", (req, res) => {
     res.writeHead(200, {
         "Connection": "keep-alive",
@@ -15,13 +19,14 @@ app.get("/synchronizetime", (req, res) => {
     setInterval(() => {sendTimeToClient(res)}, 1000);
 });
 
-// \n\n er hvad vi gerne vil slut data af med
+
 function sendTimeToClient(res) {
     const time = new Date().toISOString();
-    res.write(`data: ${time} \n\n`); // skriver heletiden, hvis der står noget andet end "data" så virker det ikke
+    //                       \n\n makes a new line
+    res.write(`data: ${time} \n\n`); // skriver heletiden - hvis der står noget andet end "data" så virker det ikke
 }
 
-const PORT = Number(process.env.PORT) || 8080; // nu kan det sættes af env værdier, eller defaulter til 8080
+const PORT = Number(process.env.PORT) || 8080; // auto 8080, hvis der ikke er en 'PORT' .env værdi
 app.listen(PORT, () => console.log("server is running on port", PORT)) ;
 
 

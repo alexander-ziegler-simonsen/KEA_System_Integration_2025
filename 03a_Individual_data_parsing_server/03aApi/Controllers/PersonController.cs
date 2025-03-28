aeef413b04c6b061b6d3bb09949204db33cc76f3
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-03-28 09:57:05 +0100

    03a - c# dotnet - start on api

---
+++ 03a_Individual_data_parsing_server/03aApi/Controllers/PersonController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace _03aApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PersonController : ControllerBase
+{
+    // private static readonly string[] Summaries = new[]
+    // {
+    //     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    // };
+
+    private readonly ILogger<PersonController> _logger;
+
+    public PersonController(ILogger<PersonController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetPeople", )]
+    public IEnumerable<Person> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new Person
+        {
+            Name = "kim",
+            Age = 14,
+            Hobbies = ["1", "2", "3"]
+        })
+        .ToArray();
+    }
+}
---
+++ 03a_Individual_data_parsing_server/03aApi/Person.cs
@@ -0,0 +1,10 @@
+namespace _03aApi;
+
+public class Person
+{
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+
+    public string[] Hobbies { get; set; }
+}

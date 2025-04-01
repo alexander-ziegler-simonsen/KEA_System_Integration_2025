3097cb6d51593a2aeaa859b6648816d55cb646a2
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-04-01 12:59:08 +0200

    00 - python - added the packages needed to run a python server

--- 00_Data_parsing_server_part_II/02_python/main.py
+++ 00_Data_parsing_server_part_II/02_python/main.py
@@ -1,2 +1,19 @@
 from my_parser import my_parser, Person
+from fastapi import FastAPI
+import requests
 
+dataParser = my_parser()
+
+
+
+app = FastAPI()
+
+@app.get("/fastapiData")
+def getFastAPIData():
+    return { "data": "Data from FastAPI" }
+
+@app.get("/requestExpressData")
+def getRequestExpressData():
+    response = requests.get("http://127.0.0.1:8080/expressData").json()
+
+    return response

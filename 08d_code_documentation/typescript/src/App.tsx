7a5c29595afefb63ec79c4cc3ee22744a004748c
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-05-12 20:47:27 +0200

    08d - demo vite react typescript project, needed for typedoc

---
+++ 08d_code_documentation/typescript/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>text goes here</p>
+    </>
+  )
+}
+
+export default App

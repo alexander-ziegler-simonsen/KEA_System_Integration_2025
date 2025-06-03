edee06b6178c583a0eb8c401593d0805ff020ada
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-06-03 22:51:25 +0200

    03a - serverA - copied the person class from old project

---
+++ 03a_Individual_data_parsing_server/serverA/src/Person.js
@@ -0,0 +1,11 @@
+export class Person {
+    constructor(nameInput, ageInput, hobbiesInput) {
+        this.name = nameInput;
+        this.age = ageInput;
+        this.hobbies = hobbiesInput;
+    }
+
+    displayPerson() {
+        console.log(`name: ${this.name} , age: ${this.age} , hobbies: ${this.hobbies.toString()}`);
+    }
+}

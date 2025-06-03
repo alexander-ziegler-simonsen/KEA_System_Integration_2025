3fa8261b23f6b8b4f43eae7fde30477ba39e1650
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-06-03 22:49:25 +0200

    03a - serverB - added constructor

--- 03a_Individual_data_parsing_server/serverB/Person.cs
+++ 03a_Individual_data_parsing_server/serverB/Person.cs
@@ -1,8 +1,19 @@
 public class Person
 {
-    public int Id { get; set; }
     public string Name { get; set; }
     public int Age { get; set; }
 
     public string[] Hobbies { get; set; }
+
+    public Person(string name, int age, string[] hobbies)
+    {
+        Name = name;
+        Age = age;
+        Hobbies = hobbies;
+    }
+
+    public Person()
+    {
+        
+    }
 }

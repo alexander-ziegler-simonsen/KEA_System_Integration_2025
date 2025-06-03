59a09c1c2db3100209a4bc2a0587e248f47c1e27
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-06-03 20:23:54 +0200

    00 - node - fixed some bugs

--- 00_Data_parsing_server_part_II/01_node/app.js
+++ 00_Data_parsing_server_part_II/01_node/app.js
@@ -13,7 +13,7 @@ app.get("/expressData", (req, res) => {
 });
 
 app.get("/requestFastApiData", async (req, res) => {
-    const response = fetch("http://127.0.0.1:8000/fastapiData");
+    const response = await fetch("http://127.0.0.1:8000/fastapiData");
     const result = await response.json();
 
     res.send({data: result.data});
